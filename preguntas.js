d1851abad3a4a23f462b60b219fef6757f1e0bc9
Matias Chaves <matiaschaves01@gmail.com>
2022-05-29 20:01:16 -0300

    initial commit

---
+++ preguntas.js
@@ -0,0 +1,50 @@
+const categorias = document.querySelectorAll('#categorias .categoria');
+const contenedorPreguntas = document.querySelectorAll('.contenedor-preguntas');
+let categoriaActiva = null;
+
+categorias.forEach((categoria) => {
+	categoria.addEventListener('click', (e) => {
+		categorias.forEach((elemento) => {
+			elemento.classList.remove('activa');
+		});
+
+		e.currentTarget.classList.toggle('activa');
+		categoriaActiva = categoria.dataset.categoria;
+
+
+		contenedorPreguntas.forEach((contenedor) => {
+			if(contenedor.dataset.categoria === categoriaActiva){
+				contenedor.classList.add('activo');
+			} else {
+				contenedor.classList.remove('activo');
+			}
+		});
+	});
+});
+
+const preguntas = document.querySelectorAll('.preguntas .contenedor-pregunta');
+preguntas.forEach((pregunta) => {
+	pregunta.addEventListener('click', (e) => {
+		e.currentTarget.classList.toggle('activa');
+
+		const respuesta = pregunta.querySelector('.respuesta');
+		const alturaRealRespuesta = respuesta.scrollHeight;
+		
+		if(!respuesta.style.maxHeight){
+			
+			respuesta.style.maxHeight = alturaRealRespuesta + 'px';
+		} else {
+			respuesta.style.maxHeight = null;
+		}
+
+		preguntas.forEach((elemento) => {
+			
+			if(pregunta !== elemento){
+				elemento.classList.remove('activa');
+				elemento.querySelector('.respuesta').style.maxHeight = null;
+			}
+		});
+ 
+
+	});  
+});
